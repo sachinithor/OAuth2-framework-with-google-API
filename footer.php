468deb0a588a27819b1158f440647fda378a5db5
sachinithor <chloethora@gmail.com>
2018-10-20 14:55:23 +0530

    change footer

--- footer.php
+++ footer.php
@@ -2,13 +2,12 @@
 		<!-- Footer -->
         <footer id="footer">
 				<ul class="icons">
-					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					<li><a href="https://sachinithorr.wordpress.com/2018/10/09/oauth-2-0-framework-with-google-api/" class="icon fa-wordpress"><span class="label">Wordpress</span></a></li>
+					<li><a href="https://github.com/sachinithor/OAuth2-framework-with-google-API" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="mailto:chloethora@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
 				</ul>
 				<ul class="copyright">
-					<li>&copy; SLIIT.</li><li>Credits: <a href="#">chloethora@gmail.com</a></li>
+					<li>&copy; SLIIT.</li><li>Credits: <a href="mailto:chloethora@gmail.com">chloethora@gmail.com</a></li>
 				</ul>
 			</footer>
 
