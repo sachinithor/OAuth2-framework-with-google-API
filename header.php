4a305c0ba9f811f1b770e28ea4fa280511e83f47
sachinithor <chloethora@gmail.com>
2018-10-20 12:11:07 +0530

    Template and HTML

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Google Driver File Upload</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<h1>Upload Your File Simply</h1>
+			</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    include_once("header.php");
+?>
+
+<!-- Signup Form -->
+<form id="signup-form" method="post" action="#">
+	<input type="submit" value="UPLOAD" />
+</form>
+
+<?php
+    include_once("footer.php");
+?>
